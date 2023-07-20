32ee9ab01b101de90eb1427b073edc83536ae1f7
jungseo <jungwook22@gmail.com>
2023-07-20 14:20:18 +0900

    first commit

---
+++ src/main/java/com/example/questionanswerboard/QuestionAnswerBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionanswerboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionAnswerBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionAnswerBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/questionanswerboard/QuestionAnswerBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionanswerboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionAnswerBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
